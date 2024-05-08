f663821465b8678be281765ca06818b67e48f4c7
labazov13 <labazopelastra@gmail.com>
2024-05-08 17:22:41 +0700

    add method sayGoodbye

--- file4.java
+++ file4.java
@@ -2,10 +2,15 @@ class file4{
     public static void main(String[] args) {
         System.out.println("HELLO");
         sayHi();
+        System.out.println(sayGoodbye());
     }
 
     public static void sayHi(){
         System.out.println("Hi");
+    }
+
+    public static String sayGoodbye(){
+        return "BYE";
     }
 }
 
