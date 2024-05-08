d5aeb6a492c5175719b1ea5c3df6f05d90ae0c4f
labazov13 <labazopelastra@gmail.com>
2024-05-08 17:20:29 +0700

    add method sayHi

--- file4.java
+++ file4.java
@@ -1,6 +1,11 @@
 class file4{
     public static void main(String[] args) {
         System.out.println("HELLO");
+        sayHi();
+    }
+
+    public static void sayHi(){
+        System.out.println("Hi");
     }
 }
 
